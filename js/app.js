1c034f06b3e7abcde2dd4aacb302dbd20c836633
bksarkar2507 <bishalkumarsarkar@gmail.com>
2022-02-28 00:33:13 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+function spinnerStatus(status){
+    document.getElementById('spinner').style.display=status;
+}
+
+// when click the search button
+function searchClick() {
+    const searchInput = document.getElementById('input');
+    const searchValue = searchInput.value;
+    spinnerStatus('block');
+    cardDiv.innerHTML = ``;
+    url(searchValue); // search value append to the url
+    searchInput.value = '';
+}
+function url(searchValue){
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${searchValue}`).then(response => response.json()).then(data => showCards(data.cards));
+}
+const cardDiv = document.createElement('div');
+const showCards = cards =>{
+    // console.log(cards);
+
+    const cardContainer = document.getElementById('card-container');
+
+    for(const card of cards){
+        // console.log(card);
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        spinnerStatus('none');
+
+        cardDiv.innerHTML = `
+            <div class="card h-50 w-50">
+                    <img src="${card.images.png}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Suit: ${card.suit}</h5> <br>
+                        <h5>Value: ${card.value}</h5> <br>
+                        <h5>Code: ${card.code}</h5> <br>
+                    </div>
+                </div>
+        `;
+        cardContainer.appendChild(cardDiv);
+    }
+}
